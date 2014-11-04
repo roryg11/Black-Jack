c7b052e07c592a79069adec259504d76b5232192
roryg11 <Rory.C.Grant@gmail.com>
2014-11-04 13:24:58 -0800

    adding card.rb

--- lib/card.rb
+++ lib/card.rb
@@ -1,4 +1,31 @@
 module BlackJack
   class Card
+
+    def initialize (suit, face)
+      @suit = suit
+      @face = face
+    end
+
+    def suit
+      @suit
+    end
+
+    def face
+      @face
+    end
+
+    def values
+      if face == 'A'
+        [1, 11]
+      elsif face == 'J' || face == 'Q' || face == 'K'
+        [10]
+      else
+        [face.to_i]
+      end
+    end
+
+    def == card_2
+      (@face == card_2.face) && (@suit == card_2.suit)
+    end
   end
 end
